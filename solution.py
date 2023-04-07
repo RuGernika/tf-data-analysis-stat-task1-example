864233b224001b45e0b504fbab73334d4597d579
Daria Tkachenko <109956269+RuGernika@users.noreply.github.com>
2023-04-07 19:15:55 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,11 +5,18 @@ import numpy as np
 chat_id = 1902092480 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    t=10 
-    return x.mean() # Ваш ответ
+    speeds = x
+    num = len(x)
+    error_dist = lambda x: -43 + np.random.exponential(1)
+    measurements =[]
+    for i in range(num):
+      measurements = speeds + np.array([error_dist(x) for x in range(num)])
+    variances = []
+    for i in range(num):
+      variances.append(1/np.exp(i+1)**2)
+    weights = 1/np.asarray(variances)
+    theta_hat = np.sum(weights * measurements) / np.sum(weights)
+    return theta_hat
 
 # 1 Условие
 # На заводе проводится тестирование модели машины для проверки коэффициента ускорения. В рамках эксперимента выбирается п машин этой модели и измеряется скорость машины через 10 секунд. Предполагается, что ошибки измерения скорости н.о.р. и имеют распределение -43 + еxp(1). Постройте точечную оценку коэффициента ускорения.
